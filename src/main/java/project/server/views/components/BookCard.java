332fe895e9f9f6789aa1d53510c5c9a1d22d97b9
Giovanni M. Leo <2232254@slu.edu.ph>
2024-03-18 10:24:00 +0800

    created some Server GUI components

---
+++ src/main/java/project/server/views/components/BookCard.java
@@ -0,0 +1,9 @@
+package project.server.views.components;
+
+/**
+ * Marker class to represent a book card
+ * */
+
+public interface BookCard {
+
+}
---
+++ src/main/java/project/server/views/components/manageBookPanel/AllBookCard.java
@@ -0,0 +1,17 @@
+package project.server.views.components.manageBookPanel;
+
+import project.server.views.components.BookCard;
+import project.utilities.viewComponents.Button;
+import project.utilities.viewComponents.Picture;
+
+import javax.swing.*;
+
+public class AllBookCard extends JPanel implements BookCard {
+
+    private Picture bookPicture;
+    private JLabel bookTitle;
+    private JLabel bookCopies;
+    private Button deleteBookButton;
+    private Button editBookButton;
+
+}
