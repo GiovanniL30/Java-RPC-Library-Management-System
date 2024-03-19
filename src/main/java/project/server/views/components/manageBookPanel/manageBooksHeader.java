7e33c78cab3714ec6b4601e142aa08ce38e0fc7e
Sean Aromin <2232315@slu.edu.ph>
2024-03-19 23:26:03 +0800

    Connected the Search bar for Manage books into the manageBooksHeader

--- src/main/java/project/server/views/components/manageBookPanel/manageBooksHeader.java
+++ src/main/java/project/server/views/components/manageBookPanel/manageBooksHeader.java
@@ -9,38 +9,28 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class manageBooksHeader extends JPanel {
-    private ClickableText pendingBorrow = new ClickableText("Pending Borrow", 100, 50, FontFactory.newPoppinsBold(18));
-    private ClickableText pendingReturn = new ClickableText("Pending Return", 100, 50, FontFactory.newPoppinsBold(18));
-    private ClickableText borrowed = new ClickableText("Borrowed", 100, 50, FontFactory.newPoppinsBold(18));
-
-    private JPanel clickablePanel;
+    private final ClickableText pendingBorrow = new ClickableText("Pending Borrow", 100, 50, FontFactory.newPoppinsBold(18));
+    private final ClickableText pendingReturn = new ClickableText("Pending Return", 100, 50, FontFactory.newPoppinsBold(18));
+    private final ClickableText borrowed = new ClickableText("Borrowed", 100, 50, FontFactory.newPoppinsBold(18));
+    private final manageBooksSearch search = new manageBooksSearch(new Dimension(200, 50));
+    private final JPanel clickablePanel;
 
     public manageBooksHeader() {
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridy = 0;
-        constraints.gridx = 0;
-
         setBorder(new EmptyBorder(0, 10, 0, 15));
-
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout());
         setSize(new Dimension(LibrarianMainFrame.WIDTH, 150));
 
         clickablePanel = new JPanel();
-        BoxLayout boxLayout = new BoxLayout(clickablePanel, BoxLayout.X_AXIS);
-        clickablePanel.setLayout(boxLayout);
+        clickablePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         clickablePanel.add(pendingBorrow);
         clickablePanel.add(pendingReturn);
         clickablePanel.add(borrowed);
 
-        add(new JPanel(), constraints);
-        constraints.gridx = 2;
-        add(new JPanel(), constraints);
-        constraints.gridx = 3;
-        add(new JPanel(), constraints);
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        searchPanel.add(search);
 
-        constraints.gridy = 4;
-        constraints.fill = 1;
-        constraints.weightx = 1.0;
-        add(clickablePanel, constraints);
+        add(clickablePanel, BorderLayout.WEST);
+        add(searchPanel, BorderLayout.EAST);
     }
 }
