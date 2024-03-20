5e824b0b9b9a13e2e89d02def4683bcc7a0768a9
Judrey Padsuyan <2232249@slu.edu.ph>
2024-03-20 21:59:26 +0800

    added BookSummary on the HomePanel

--- src/main/java/project/server/views/panels/HomePanel.java
+++ src/main/java/project/server/views/panels/HomePanel.java
@@ -1,4 +1,22 @@
 package project.server.views.panels;
 
-public class HomePanel {
+import project.server.controller.ServerObserver;
+import project.server.views.components.homePanel.BookSummary;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HomePanel extends JPanel {
+    private final ServerObserver serverController;
+
+    public HomePanel(ServerObserver serverObserver) {
+        serverController = serverObserver;
+        setLayout(new BorderLayout());
+
+        // Create the BookSummary component
+        BookSummary bookSummary = new BookSummary(serverController);
+
+        // Add the BookSummary component to the HomePanel
+        add(bookSummary, BorderLayout.SOUTH);
+    }
 }
