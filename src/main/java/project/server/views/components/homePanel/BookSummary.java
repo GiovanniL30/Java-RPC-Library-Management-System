c23589b1970c2da55cd4cc824bc71fa1a0b60e25
Judrey Padsuyan <2232249@slu.edu.ph>
2024-03-20 21:59:01 +0800

    created changes on the font size, font color and added border on BookSummary.java

--- src/main/java/project/server/views/components/homePanel/BookSummary.java
+++ src/main/java/project/server/views/components/homePanel/BookSummary.java
@@ -4,6 +4,7 @@ import project.server.controller.ServerObserver;
 import project.utilities.utilityClasses.FontFactory;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class BookSummary extends JPanel {
@@ -13,8 +14,12 @@ public class BookSummary extends JPanel {
     public BookSummary(ServerObserver serverObserver) {
         serverController = serverObserver;
         setLayout(new BorderLayout());
+        setBorder(new EmptyBorder(10, 10, 10, 10));
 
         summaryPanel = new JPanel();
+        summaryPanel.setLayout(new BoxLayout(summaryPanel, BoxLayout.Y_AXIS));
+
+        summaryPanel.add(Box.createVerticalStrut(10));
         JTextArea summaryTitle = new JTextArea("\tSummary Report");
                 JTextArea summary = new JTextArea("\n" +
                         "Number of Available Books: "
@@ -36,21 +41,20 @@ public class BookSummary extends JPanel {
         summaryTitle.setOpaque(false);
         summaryTitle.setEditable(false);
         summaryTitle.setWrapStyleWord(true);
-        summaryTitle.setForeground(Color.white);
-        summaryTitle.setTabSize(3);
+        summaryTitle.setForeground(Color.black);
 
         // Edit summary panel text
-        summary.setFont(FontFactory.newPoppinsDefault(35));
+        summary.setFont(FontFactory.newPoppinsDefault(25));
         summary.setOpaque(false);
         summary.setEditable(false);
         summary.setWrapStyleWord(true);
-        summary.setForeground(Color.white);
+        summary.setForeground(Color.black);
 
         // Add the components to the summary panel
         summaryPanel.add(summaryTitle);
         summaryPanel.add(summary);
         JScrollPane scrollPane = new JScrollPane(summaryPanel);
-        scrollPane.setPreferredSize(new Dimension(1000, 720));
+        scrollPane.setPreferredSize(new Dimension(1000, 400));
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         add(scrollPane, BorderLayout.SOUTH);
     }
