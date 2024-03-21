ee71f894655eb20dab3c399303f69d18a410c29b
Judrey Padsuyan <2232249@slu.edu.ph>
2024-03-21 10:23:25 +0800

    added temporary banner on the homePanel, created changes on the BookSummary.java on the alignment and font sizes, created changes on the HomePanel by adding Scroll Pane

--- src/main/java/project/server/views/components/homePanel/BookSummary.java
+++ src/main/java/project/server/views/components/homePanel/BookSummary.java
@@ -21,8 +21,14 @@ public class BookSummary extends JPanel {
 
         JLabel header = new JLabel("Summary Report");
         JLabel availableBooks = new JLabel("Number of Available Books: " + serverController.getAvailableBooks().size());
+        JLabel unavailableBooks = new JLabel("Number of Unavailable Books: " + serverController.getUnavailableBooks().size());
+        JLabel pendingBorrowingBooks = new JLabel("Number of Pending Books for Borrowing: " + serverController.getPendingBorrowingBooks().size());
+        JLabel pendingReturningBooks = new JLabel("Number of Pending Books for Returning: " + serverController.getPendingReturningBooks().size());
+        JLabel currentBorrowedBooks = new JLabel("Number of Current Borrowed Books: " + serverController.getCurrentBorrowedBooks().size());
+        JLabel previousBorrowedBooks = new JLabel("Number of Previous Borrowed Books: " + serverController.getPreviousBorrowedBooks().size());
+        summaryPanel.add(Box.createVerticalStrut(10));
+        summaryPanel.setPreferredSize(new Dimension(1000,400));
 
-//        summaryPanel.add(Box.createVerticalStrut(10));
 //        JTextArea summaryTitle = new JTextArea("\tSummary Report");
 //                JTextArea summary = new JTextArea("\n" +
 //                        "Number of Available Books: "
@@ -39,12 +45,31 @@ public class BookSummary extends JPanel {
 //                        + serverController.getPreviousBorrowedBooks().size()
 //                );
 
+        header.setFont(FontFactory.newPoppinsBold(30));
+        availableBooks.setFont(FontFactory.newPoppinsDefault(25));
+        unavailableBooks.setFont(FontFactory.newPoppinsDefault(25));
+        pendingBorrowingBooks.setFont(FontFactory.newPoppinsDefault(25));
+        pendingReturningBooks.setFont(FontFactory.newPoppinsDefault(25));
+        currentBorrowedBooks.setFont(FontFactory.newPoppinsDefault(25));
+        previousBorrowedBooks.setFont(FontFactory.newPoppinsDefault(25));
+
+        header.setAlignmentX(CENTER_ALIGNMENT);
+        availableBooks.setAlignmentX(CENTER_ALIGNMENT);
+        unavailableBooks.setAlignmentX(CENTER_ALIGNMENT);
+        pendingBorrowingBooks.setAlignmentX(CENTER_ALIGNMENT);
+        pendingReturningBooks.setAlignmentX(CENTER_ALIGNMENT);
+        currentBorrowedBooks.setAlignmentX(CENTER_ALIGNMENT);
+        previousBorrowedBooks.setAlignmentX(CENTER_ALIGNMENT);
 
 
+        summaryPanel.add(header);
+        summaryPanel.add(availableBooks);
+        summaryPanel.add(unavailableBooks);
+        summaryPanel.add(pendingBorrowingBooks);
+        summaryPanel.add(pendingReturningBooks);
+        summaryPanel.add(currentBorrowedBooks);
+        summaryPanel.add(previousBorrowedBooks);
 
-        JScrollPane scrollPane = new JScrollPane(summaryPanel);
-        scrollPane.setPreferredSize(new Dimension(1000, 400));
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        add(scrollPane, BorderLayout.CENTER);
+        add(summaryPanel, BorderLayout.CENTER);
     }
 }
