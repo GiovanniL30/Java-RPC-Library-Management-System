41fb7dd2704f56efb26d7144d520b96e00bba9fe
Sean Aromin <2232315@slu.edu.ph>
2024-03-21 23:45:53 +0800

    Added setCurrentButton Method

--- src/main/java/project/server/views/components/manageBookPanel/ManageBooksHeader.java
+++ src/main/java/project/server/views/components/manageBookPanel/ManageBooksHeader.java
@@ -1,20 +1,24 @@
 package project.server.views.components.manageBookPanel;
 
+import project.server.controller.ServerController;
 import project.server.views.LibrarianMainFrame;
 import project.server.views.components.ClickableText;
+import project.server.views.utility.ServerPanels;
 import project.utilities.utilityClasses.FontFactory;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.LinkedList;
 
 public class ManageBooksHeader extends JPanel {
+    private LinkedList<ClickableText> clickableTexts = new LinkedList<>();
     private final ClickableText pendingBorrow = new ClickableText("Pending Borrow", 100, 50, FontFactory.newPoppinsBold(18));
     private final ClickableText pendingReturn = new ClickableText("Pending Return", 100, 50, FontFactory.newPoppinsBold(18));
     private final ClickableText borrowed = new ClickableText("Borrowed", 100, 50, FontFactory.newPoppinsBold(18));
     private final JPanel clickablePanel;
 
-    public ManageBooksHeader() {
+    public ManageBooksHeader(ServerController serverController) {
         setBorder(new EmptyBorder(0, 10, 0, 15));
         setLayout(new BorderLayout());
         setSize(new Dimension(LibrarianMainFrame.WIDTH, 150));
@@ -27,5 +31,19 @@ public class ManageBooksHeader extends JPanel {
 
         add(clickablePanel);
 
+        borrowed.addActionListener( e -> serverController.changeFrame(ServerPanels.BORROWED_PANEL));
     }
+    public void setCurrentButton(ClickableText currentText) {
+
+        currentText.setEnabled(false);
+
+
+        for(ClickableText clickableText: clickableTexts) {
+
+            if(!clickableText.equals(currentText)){
+                clickableText.setEnabled(true);
+            }
+        }
+    }
+
 }
