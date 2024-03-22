57153b108c07c93f1a199ed3498fea00f9b9c09b
Sean Aromin <2232315@slu.edu.ph>
2024-03-22 21:26:01 +0800

    Added Action listeners to change panels within ManageBookPanel

--- src/main/java/project/server/views/components/manageBookPanel/ManageBooksHeader.java
+++ src/main/java/project/server/views/components/manageBookPanel/ManageBooksHeader.java
@@ -3,6 +3,7 @@ package project.server.views.components.manageBookPanel;
 import project.server.controller.ServerController;
 import project.server.views.LibrarianMainFrame;
 import project.server.views.components.ClickableText;
+import project.server.views.panels.ManageBookPanel;
 import project.server.views.utility.ServerPanels;
 import project.utilities.utilityClasses.FontFactory;
 
@@ -12,38 +13,39 @@ import java.awt.*;
 import java.util.LinkedList;
 
 public class ManageBooksHeader extends JPanel {
-    private LinkedList<ClickableText> clickableTexts = new LinkedList<>();
-    private final ClickableText pendingBorrow = new ClickableText("Pending Borrow", 100, 50, FontFactory.newPoppinsBold(18));
-    private final ClickableText pendingReturn = new ClickableText("Pending Return", 100, 50, FontFactory.newPoppinsBold(18));
-    private final ClickableText borrowed = new ClickableText("Borrowed", 100, 50, FontFactory.newPoppinsBold(18));
-    private final JPanel clickablePanel;
+    private final ServerController serverController;
+    private final ManageBookPanel manageBookPanel; // Reference to ManageBookPanel
+    private final ClickableText pendingBorrow;
+    private final ClickableText pendingReturn;
+    private final ClickableText borrowed;
 
-    public ManageBooksHeader(ServerController serverController) {
+    public ManageBooksHeader(ServerController serverController, ManageBookPanel manageBookPanel) {
+        this.serverController = serverController;
+        this.manageBookPanel = manageBookPanel; // Assign the reference
+
         setBorder(new EmptyBorder(0, 10, 0, 15));
         setLayout(new BorderLayout());
         setSize(new Dimension(LibrarianMainFrame.WIDTH, 150));
 
-        clickablePanel = new JPanel();
+        JPanel clickablePanel = new JPanel();
         clickablePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        pendingBorrow = new ClickableText("Pending Borrow", 100, 50, FontFactory.newPoppinsBold(18));
+        pendingReturn = new ClickableText("Pending Return", 100, 50, FontFactory.newPoppinsBold(18));
+        borrowed = new ClickableText("Borrowed", 100, 50, FontFactory.newPoppinsBold(18));
+
         clickablePanel.add(pendingBorrow);
         clickablePanel.add(pendingReturn);
         clickablePanel.add(borrowed);
 
-        add(clickablePanel);
+        add(clickablePanel, BorderLayout.NORTH);
 
-        borrowed.addActionListener( e -> serverController.changeFrame(ServerPanels.BORROWED_PANEL));
-    }
-    public void setCurrentButton(ClickableText currentText) {
-
-        currentText.setEnabled(false);
-
-
-        for(ClickableText clickableText: clickableTexts) {
-
-            if(!clickableText.equals(currentText)){
-                clickableText.setEnabled(true);
-            }
-        }
+        pendingBorrow.addActionListener(e -> switchToPanel(ServerPanels.PENDING_BORROW_PANEL));
+        pendingReturn.addActionListener(e -> switchToPanel(ServerPanels.PENDING_RETURN_PANEL));
+        borrowed.addActionListener(e -> switchToPanel(ServerPanels.BORROWED_PANEL));
     }
 
+    private void switchToPanel(ServerPanels panel) {
+        manageBookPanel.switchToPanel(panel); // Call switchToPanel of ManageBookPanel
+    }
 }
