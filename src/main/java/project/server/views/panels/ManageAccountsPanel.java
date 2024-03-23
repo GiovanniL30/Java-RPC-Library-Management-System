9e40d3c3da5b08c4134054b5338e054ca61f9d65
Jhea Jhana Prudencio <2232799@slu.edu.ph>
2024-03-23 18:31:05 +0800

    created AccountCard in ManageAccountsPanel

--- src/main/java/project/server/views/panels/ManageAccountsPanel.java
+++ src/main/java/project/server/views/panels/ManageAccountsPanel.java
@@ -4,6 +4,7 @@ import project.server.controller.ServerController;
 import project.server.views.components.ClickableText;
 import project.utilities.referenceClasses.Student;
 import project.utilities.utilityClasses.FontFactory;
+import project.utilities.viewComponents.Picture;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -51,7 +52,92 @@ public class ManageAccountsPanel extends JPanel{
 
     private class AccountCard extends JPanel{
         AccountCard(Student account) {
+            setBorder(new EmptyBorder(20, 0, 0, 0));
+            setMaximumSize(new Dimension(500, 400));
+            setMaximumSize(getPreferredSize());
 
+            JPanel buttonsPanel = new JPanel();
+            GridLayout buttonsGrid = new GridLayout(2, 2);
+            buttonsGrid.setHgap(10);
+            buttonsGrid.setVgap(10);
+            buttonsPanel.setLayout(buttonsGrid);
+
+            JPanel accountInfo = new JPanel();
+            accountInfo.setLayout(new GridLayout(3, 1));
+
+            Button banButton = new Button("Ban");
+            banButton.setForeground(Color.white);
+            banButton.setBackground(Color.getColor("7825C6FF"));
+
+            Button unbanButton = new Button("Unban");
+            unbanButton.setForeground(Color.white);
+            unbanButton.setBackground(Color.getColor("57955CFF"));
+
+            Button deleteAccount = new Button("Delete");
+            deleteAccount.setForeground(Color.white);
+            deleteAccount.setBackground(Color.getColor("DA5C5CFF"));
+
+            Button editAccount = new Button("Edit Pass");
+            editAccount.setForeground(Color.WHITE);
+            editAccount.setBackground(Color.getColor("3163CDFF"));
+
+            // Creating labels for account information
+            JLabel userName = new JLabel("Username: " + account.getAccount().getUserName());
+            JLabel name = new JLabel("Name: " + account.getAccount().getFirstName() + " " + account.getAccount().getLastName());
+            JLabel userPassword = new JLabel("Password: " + account.getAccount().getPassword());
+
+            // Add buttons to buttons panel
+            buttonsPanel.add(unbanButton);
+            buttonsPanel.add(banButton);
+            buttonsPanel.add(editAccount);
+            buttonsPanel.add(deleteAccount);
+            buttonsPanel.setBorder(new EmptyBorder(10, 0, 0, 0));
+
+            // Add account info to account info panel
+            accountInfo.add(name);
+            accountInfo.add(userName);
+            accountInfo.add(userPassword);
+
+            JPanel rightSide = new JPanel();
+            rightSide.setLayout(new BoxLayout(rightSide, BoxLayout.Y_AXIS));
+            rightSide.add(accountInfo);
+            rightSide.add(buttonsPanel);
+
+            // Creating picture label for account image
+            Picture picture = new Picture("res/icons/profileAccount.jpg", 170, 170);
+            // Setting color and button states based on account status
+            if (account.getAccount().isBanned()) {
+                picture.setBackground(Color.red);
+                banButton.setEnabled(false);
+                banButton.setBackground(Color.white);
+            } else {
+                picture.setBackground(Color.getColor("57955CFF"));
+                unbanButton.setEnabled(false);
+                unbanButton.setBackground(Color.white);
+            }
+
+            rightSide.setBackground(new Color(238, 238, 238));
+            rightSide.setBorder(new EmptyBorder(0, 10, 0, 0));
+            add(picture);
+
+            add(rightSide);
+
+            // Action listeners for banning, unbanning, deleting, and edting account
+            banButton.addActionListener(e -> serverController.banAccount(account.getAccount()));
+            unbanButton.addActionListener(e -> serverController.unbanAccount(account.getAccount()));
+            deleteAccount.addActionListener(e -> serverController.deleteAccount(account));
+            editAccount.addActionListener(e -> {
+                String newPass = JOptionPane.showInputDialog(null, "Enter the new password: ");
+                if (newPass == null || newPass.isEmpty()) return;
+
+                if (newPass.length() < 8) {
+                    JOptionPane.showMessageDialog(null, "Password Length Invalid [8 and above]");
+                    return;
+                }
+
+                serverController.changeUserPassword(account.getAccount(), newPass);
+            });
+            //clickableText.addActionListener(e -> signup.setVisible(true));
         }
     }
 }
