ce9d035d7e4624502fb0e74ae09e947d8708c68d
Eugene <2233071@slu.edu.ph>
2024-03-24 01:04:59 +0800

    Created AvailableBooks and UnavailableBooks Panel

--- src/main/java/project/server/views/components/viewBookPanel/AvailableBooksPanel.java
+++ src/main/java/project/server/views/components/viewBookPanel/AvailableBooksPanel.java
@@ -1,4 +1,17 @@
 package project.server.views.components.viewBookPanel;
 
-public class AvailableBooksPanel {
+import project.server.controller.ServerController;
+import javax.swing.*;
+import java.awt.*;
+
+public class AvailableBooksPanel extends JPanel {
+    private final ServerController serverController;
+    private ViewBooksList bookList ;
+    public AvailableBooksPanel(ServerController serverController) {
+        this.serverController = serverController;
+
+        setBackground(Color.white);
+        bookList = new ViewBooksList(serverController.getAvailableBooks(), serverController);
+        add(bookList);
+    }
 }
--- src/main/java/project/server/views/components/viewBookPanel/UnavailableBooksPanel.java
+++ src/main/java/project/server/views/components/viewBookPanel/UnavailableBooksPanel.java
@@ -1,4 +1,17 @@
 package project.server.views.components.viewBookPanel;
 
-public class UnavailableBooksPanel {
+import project.server.controller.ServerController;
+import javax.swing.*;
+import java.awt.*;
+
+public class UnavailableBooksPanel extends JPanel {
+    private final ServerController serverController;
+    private ViewBooksList bookList ;
+    public UnavailableBooksPanel(ServerController serverController) {
+        this.serverController = serverController;
+
+        setBackground(Color.white);
+        bookList = new ViewBooksList(serverController.getUnavailableBooks(), serverController);
+        add(bookList);
+    }
 }
