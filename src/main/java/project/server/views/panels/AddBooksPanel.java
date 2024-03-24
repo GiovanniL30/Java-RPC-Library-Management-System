97b799905cf0c4e60ba3b04517835e17f4cb768a
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-03-24 11:39:40 +0800

    added AddBooksPanel

---
+++ src/main/java/project/server/views/panels/AddBooksPanel.java
@@ -0,0 +1,11 @@
+package project.server.views.panels;
+
+import project.server.controller.ServerController;
+
+import javax.swing.*;
+
+public class AddBooksPanel extends JPanel {
+    public AddBooksPanel(ServerController serverController){
+
+    }
+}
