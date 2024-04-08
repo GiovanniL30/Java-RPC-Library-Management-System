2d44696dad1c9a5aa3eb8a13934294868cb669b1
Jhea Jhana Prudencio <2232799@slu.edu.ph>
2024-04-08 20:38:31 +0800

    Completed Server package

--- src/main/java/midproject/server/AdminApplication.java
+++ src/main/java/midproject/server/AdminApplication.java
@@ -7,24 +7,31 @@ import midproject.server.views.LibrarianMainFrame;
 import javax.swing.*;
 import java.rmi.RemoteException;
 
+/**
+ * Entry point for the administrator application.
+ */
+
 public class AdminApplication {
-
+    /**
+     * Main method
+     */
     public static void main(String[] args) {
-
         try {
-
+            // Create a new instance of the server controller
             ServerController serverController = new ServerController();
+            // Create a new instance of the server updates
             ServerUpdates servant = new ServerUpdates(serverController);
+            // Set the server methods
             serverController.setServerMethods(servant);
 
+            // Create and display the main frame on the event dispatch thread
             SwingUtilities.invokeLater(() -> {
                 LibrarianMainFrame mainFrame = new LibrarianMainFrame(serverController);
                 serverController.setServerMainView(mainFrame);
             });
-
         } catch (RemoteException e) {
+            // If an exception occurs, throw a runtime exception
             throw new RuntimeException(e);
         }
-    }
-
-}
+    } // end of main method
+} // end of class
