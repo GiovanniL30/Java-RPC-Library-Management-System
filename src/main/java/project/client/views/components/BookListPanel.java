1702960267e4505e3a83f0288f4bf25a7eccbd0b
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-04-08 19:15:39 +0800

    add comments for BookListPanel

--- src/main/java/project/client/views/components/BookListPanel.java
+++ src/main/java/project/client/views/components/BookListPanel.java
@@ -12,40 +12,63 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.util.LinkedList;
 
+/**
+ * Class that shows the books in a JPanel
+ */
 public class BookListPanel extends JPanel {
 
     private final boolean isPending;
     private final ClientController clientController;
 
+    /**
+     * Constructor
+     * @param pendingBooks pending books of a user
+     * @param clientController controller object
+     * @param isPending boolean value if the books are pending
+     * @param height used in setting the size of the panel
+     */
     public BookListPanel(LinkedList<Book> pendingBooks, ClientController clientController, boolean isPending, int height) {
 
         this.isPending = isPending;
         this.clientController = clientController;
 
+        // instantiate a panel to hold other components
         JPanel holder = new JPanel();
         GridLayout gridLayout = new GridLayout(0, 1);
         gridLayout.setVgap(20);
+
+        // set the layout of the holder as GridLayout
         holder.setLayout(gridLayout);
 
         for (Book book : pendingBooks) {
+            // add each pending book to the panel
             holder.add(new BookCard(book));
-        }
+        } // end of for each
 
         JScrollPane scrollPane = new JScrollPane(holder);
         scrollPane.setPreferredSize(new Dimension(900, height));
 
         add(scrollPane);
 
-    }
+    } // end of constructor
 
+    /**
+     * Book Cards of each book to be added to the holder panel
+     */
     private class BookCard extends JPanel {
 
+        /**
+         * Constructor
+         * @param book book to be made into a book card
+         */
         private BookCard(Book book) {
 
+            // set the book card's properties
             setSize(new Dimension(300, 200));
             setLayout(new GridBagLayout());
             setBorder(new EmptyBorder(0, 10, 0, 20));
 
+            // fetch components to be put in the book card
             Picture picture = new Picture(book.getImagePath(), 130, 200);
             JLabel bookName = new JLabel(book.getBookTitle());
             bookName.setFont(FontFactory.newPoppinsBold(18));
@@ -53,15 +76,17 @@ public class BookListPanel extends JPanel {
             author.setFont(FontFactory.newPoppinsDefault(12));
 
             Button button;
+            // set the button's text depending on the status of the book
             if (isPending) {
                 button = new Button("Cancel", 100, 50, FontFactory.newPoppinsDefault(13));
             } else {
                 button = new Button("Return", 100, 50, FontFactory.newPoppinsDefault(13));
-            }
+            } // end of if else
 
             button.setForeground(Color.white);
             button.setBackground(ColorFactory.red());
 
+            // instantiate panels and add their components
             JPanel bookInfoPanel = new JPanel();
             JPanel bookPanel = new JPanel();
 
@@ -79,11 +104,14 @@ public class BookListPanel extends JPanel {
             constraints.weightx = 2.0;
             constraints.fill = 2;
 
+            // add the book panel using the given constraints
             add(bookPanel, constraints);
 
             constraints.gridx = 1;
             constraints.weightx = 0.0;
             constraints.fill = 0;
+
+            // add the buttons using the given constraints
             add(button, constraints);
 
 
@@ -93,24 +121,26 @@ public class BookListPanel extends JPanel {
 
                     int option = JOptionPane.showConfirmDialog(clientController.getMainView(), "Are you sure you want to cancel " + book.getBookTitle() + "?", "Confirm Cancel", JOptionPane.YES_NO_OPTION);
                     if (option == JOptionPane.YES_OPTION) {
+                        // remove the book from pending
                         clientController.removePending(book);
-                    }
+                    } // end of if statement
 
 
                 } else {
                     int option = JOptionPane.showConfirmDialog(clientController.getMainView(), "Are you sure you want to cancel " + book.getBookTitle() + "?", "Confirm Cancel", JOptionPane.YES_NO_OPTION);
                     if (option == JOptionPane.YES_OPTION) {
+                        // return the book
                         clientController.returnBook(book);
-                    }
-                }
+                    } // end of if statement
+                } // end of if else
 
             });
 
 
-        }
+        } // end of BookCard constructor
 
 
-    }
+    } // end of BookCard class
 
 
-}
+} // end of class
