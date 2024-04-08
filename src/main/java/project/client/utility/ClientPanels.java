14ff546408ec150d075731c812cb97a95688c34e
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-04-08 18:56:59 +0800

    add comments for ClientPanels

--- src/main/java/project/client/utility/ClientPanels.java
+++ src/main/java/project/client/utility/ClientPanels.java
@@ -1,8 +1,12 @@
 package project.client.utility;
 
+/**
+ * Enum class for available panels for clients
+ */
 public enum ClientPanels {
+
     HOME_PANEL,
     PENDING_PANEL,
     BORROWED_PANEL,
     ACCOUNT_PANEL
-}
+} // end of ClientPanels
