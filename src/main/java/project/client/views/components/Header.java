a23952fc6951838a02e0ecd49764cc9f13a5e40d
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-04-08 20:44:38 +0800

    add comments for multiple classes

--- src/main/java/project/client/views/components/Header.java
+++ src/main/java/project/client/views/components/Header.java
@@ -13,25 +13,36 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+/**
+ * Class that functions as a header
+ */
 public class Header extends JPanel {
 
     private final Button logout;
     private final IconButton message;
 
+    /**
+     * Constructor
+     * @param studentName name of the student
+     */
     public Header(String studentName) {
         setBackground(ColorFactory.blue());
 
         GridBagConstraints constraints = new GridBagConstraints();
 
+        // set the properties of this panel
         setPreferredSize(new Dimension(ClientMainView.FRAME_WIDTH, 100));
         setLayout(new GridBagLayout());
         setBorder(new EmptyBorder(0, 10, 0, 30));
 
+        // set the components to be added to the panel
         Picture logo = new Picture("src/main/resources/images/logo/logo_vanni.png", 100, 100);
         logo.setBackground(ColorFactory.blue());
         JLabel label = new JLabel(studentName.toUpperCase());
         label.setForeground(Color.WHITE);
         label.setFont(FontFactory.newPoppinsBold(18));
+
+        // initialize the logout button and its properties
         logout = new Button("LOGOUT", 150, 50, FontFactory.newPoppinsBold(13));
         logout.setForeground(Color.WHITE);
         logout.setBackground(ColorFactory.red());
@@ -41,11 +52,15 @@ public class Header extends JPanel {
 
         constraints.gridy = 0;
         constraints.gridx = 0;
+
+        // add the logo using the given constraints
         add(logo,constraints);
 
         constraints.fill = 2;
         constraints.weightx = 2.0;
         constraints.gridx = 1;
+
+        // add the label using the given constraints
         add(label, constraints);
 
 
@@ -53,19 +68,31 @@ public class Header extends JPanel {
         constraints.weightx = 0.0;
         constraints.fill = 0;
         constraints.insets = new Insets(0, 0, 0, 15);
+
+        // add the message using the given constraints
         add(message, constraints);
 
         constraints.gridx = 3;
         constraints.insets = new Insets(0, 0, 0, 0);
-        add(logout, constraints);
-    }
 
+        // add the logout button using the given constraints
+        add(logout, constraints);
+    } // end of constructor
+
+    /**
+     * Adds the action for the logout button
+     * @param clientController
+     */
     public void addLogoutAction(ClientController clientController) {
         logout.addActionListener(e -> clientController.logout());
-    }
+    } // end of addLogoutAction
 
+    /**
+     * Adds the action for the message button
+     * @param clientController
+     */
     public void addMessageAction(ClientController clientController) {
         message.addActionListener(e -> clientController.openMessageChat());
-    }
+    } // end of addMessageAction
 
-}
+} // end of class
--- src/main/java/project/client/views/components/HomePanel.java
+++ src/main/java/project/client/views/components/HomePanel.java
@@ -15,6 +15,9 @@ import java.awt.event.ItemEvent;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 
+/**
+ * Class that shows the home panel
+ */
 public class HomePanel extends JPanel {
 
     private final LinkedList<Book> books;
@@ -24,11 +27,18 @@ public class HomePanel extends JPanel {
     private boolean haveSearched = false;
     private  ClientController clientController;
 
+    /**
+     * Constructor
+     * @param books books to be put in the book list
+     * @param clientController controller
+     */
     public HomePanel(LinkedList<Book> books, ClientController clientController) {
+        // sets the layout of the panel into BoxLayout
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.books = books;
         this.clientController = clientController;
 
+        // instantiate the components in the panel
         searchBar = new SearchBar(new Dimension(800, 100), false);
 
         GridLayout gridLayout = new GridLayout(0, 2);
@@ -47,6 +57,7 @@ public class HomePanel extends JPanel {
         searchButton.setBackground(ColorFactory.blue());
         searchButton.setForeground(Color.white);
 
+        // add the header into the panel
         add(header);
 
         panel = new JPanel();
@@ -55,14 +66,20 @@ public class HomePanel extends JPanel {
         scrollPane.setPreferredSize(new Dimension(920, 400));
         add(scrollPane);
 
+        // populate the book list using the entered parameter
         populateBookList(books, false);
 
-
+        // add action listeners for buttons
         searchButton.addActionListener(this::performSearch);
         searchBar.getGenreDropDown().addItemListener(this::performGenreFiltering);
         searchBar.getCancelButton().addActionListener(this::closeSearch);
-    }
+    } // end of constructor
 
+    /**
+     * Populates the book list
+     * @param books books to be put in the book list
+     * @param isUpdate value to check if it is used for updates
+     */
     public void populateBookList(LinkedList<Book> books, boolean isUpdate) {
         new SwingWorker<>() {
             @Override
@@ -70,78 +87,100 @@ public class HomePanel extends JPanel {
 
                 if (isUpdate) {
                     panel.removeAll();
-                }
+                } // end of if statement
 
                 if(books.isEmpty()) {
                     panel.revalidate();
                     panel.repaint();
-                }
+                } // end of if statement
 
                 for (Book book : books) {
                     panel.add(new BookCardComponent(book, clientController));
                     panel.revalidate();
                     panel.repaint();
-                }
+                } // end of for each
 
                 return null;
-            }
+            } // end of doInBackground
 
 
         }.execute();
-    }
+    } // end of populateBookList
 
+    /**
+     * Closes the search function
+     * @param event
+     */
     private void closeSearch(ActionEvent event){
 
         if(haveSearched) {
             populateBookList(books, true);
             searchBar.getInput().getTextField().setText("");
             haveSearched = false;
-        }
+        } // end of if statement
 
-    }
+    } // end of closeSearch
 
+    /**
+     * Searches the books using the input
+     * @param event
+     */
     private void performSearch(ActionEvent event) {
 
         String searchInput = searchBar.getSearch();
 
+        // handle errors
         if(searchInput == null){
             searchBar.getInput().enableError("Enter something");
             return;
-        }
+        } // end of if statement
 
         if(searchInput.trim().isEmpty()) {
             searchBar.getInput().enableError("Enter something");
             return;
-        }
+        } // end of if statement
 
         String filterType = searchBar.getFilterType();
 
         LinkedList<Book> filteredBooks;
 
+        // search the books with the author filter
         if(filterType.equals("Author")){
             filteredBooks = books.stream().filter(book -> book.getAuthor().toLowerCase().contains(searchInput.toLowerCase())).collect(Collectors.toCollection(LinkedList::new));
         }else {
             filteredBooks = books.stream().filter(book -> book.getBookTitle().toLowerCase().contains(searchInput.toLowerCase())).collect(Collectors.toCollection(LinkedList::new));
-        }
+        } // end of if else
 
 
         haveSearched = true;
         populateBookList(filteredBooks, true);
-    }
+    } // end of performSearch
 
+    /**
+     * Filter books according to their genres
+     * @param e
+     */
     public void performGenreFiltering(ItemEvent e) {
         LinkedList<Book> filtered = books.stream().filter(book -> book.getGenre().toLowerCase().contains(searchBar.getGenreDropDown().dropDownChoice().toLowerCase())).collect(Collectors.toCollection(LinkedList::new));
         haveSearched = true;
         populateBookList(filtered, true);
-    }
+    } // end of performGenreFiltering
 
-
+    /**
+     * Class for book card
+     */
     private static class BookCardComponent extends JPanel {
 
+        /**
+         * Constructor
+         * @param book
+         * @param clientController
+         */
         private BookCardComponent(Book book, ClientController clientController) {
 
             setSize(new Dimension(100, 200));
 
+            // set the components' properties
             IconButton button = new IconButton(book.getImagePath(), 250, 300);
             JLabel label = new JLabel(book.getBookTitle());
             label.setFont(FontFactory.newPoppinsBold(16));
@@ -151,13 +190,17 @@ public class HomePanel extends JPanel {
             GridBagConstraints constraints = new GridBagConstraints();
             constraints.gridy = 0;
             constraints.gridx = 0;
+
+            // add the button using the given constraints
             add(button, constraints);
             constraints.gridy = 1;
+
+            // add the label using the given constraints
             add(label, constraints);
 
             button.addActionListener(e -> clientController.openBook(book));
-        }
+        } // end of constructor
 
-    }
+    } // end of BookCardComponent
 
-}
+} // end of class
