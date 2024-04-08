8c43811daf44b10534dd3d44af9de22fa2e8d67c
Jhea Jhana Prudencio <2232799@slu.edu.ph>
2024-04-08 19:00:58 +0800

    Finished comments in model package

--- src/main/java/project/server/model/DataModel.java
+++ src/main/java/project/server/model/DataModel.java
@@ -25,15 +25,15 @@ import java.util.stream.Collectors;
  * It provides features or the system and also handles operations.
  */
 public class DataModel {
-
-
+    //File paths for JSON data
     protected final String accountJSONfilePath = "src/main/resources/data/account.json";
     protected final String bookJSONPath = "src/main/resources/data/book.json";
     protected Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-
+    /**
+     * Saves JSON data to a file
+     */
     protected synchronized void saveJSON(String json, String filePath) {
-
         try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(json);
             System.out.println("Successfully wrote JSON to file.");
@@ -42,9 +42,10 @@ public class DataModel {
         }
     }
 
-
+    /**
+     * Retrieves the list of all books from JSON file
+     */
     public synchronized LinkedList<Book> getBooks() {
-
         try (FileReader reader = new FileReader("src/main/resources/data/book.json")) {
             Type linkedListType = new TypeToken<LinkedList<TransformedBook>>() {}.getType();
             LinkedList<TransformedBook> transformedBooks = gson.fromJson(reader, linkedListType);
@@ -53,77 +54,77 @@ public class DataModel {
             LinkedList<Student> students = getStudentAccounts(books);
 
             for(TransformedBook transformedBook : transformedBooks) {
-
+                // Transform data from transformedBook to Book
                 LinkedList<String> currentBorrowers = transformedBook.getCurrentBorrowers().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
                 LinkedList<PrevBorrower> prevBorrowers = new LinkedList<>();
 
                 transformedBook.getPrevBookBorrowers().forEach(current -> {
 
                     for (Student student : students) {
-
                         if (student.getAccount().getAccountId().equals(current.get("student").toString().replace("\"", ""))) {
                             prevBorrowers.add(new PrevBorrower(current.get("returnDate").toString().replace("\"", ""), current.get("student").getAsString(), current.get("name").getAsString()));
                         }
 
                     }
                 });
-
                 LinkedList<String> pendingReturn = transformedBook.getPendingBookReturners().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
                 LinkedList<String> pendingBorrow = transformedBook.getPendingBorrowers().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
                 books.add(new Book(transformedBook.getBookId(), transformedBook.getBookTitle(), transformedBook.getAuthor(), transformedBook.getGenre(), transformedBook.getShortDescription(), transformedBook.getImagePath(), transformedBook.getCopies(), currentBorrowers, prevBorrowers, pendingBorrow, pendingReturn));
            }
-
-
-
-
-
             return books;
         } catch (IOException e) {
             return new LinkedList<>();
         }
-
     }
 
+    /**
+     * Adds a pending book request for a student
+     */
     public boolean addPending(String bookId, Student student) {
-
         LinkedList<Book> books = getBooks();
 
         for (Book book : books) {
             if (book.getBookId().equals(bookId)) {
+                // decrease available copies, add student to pending list
                 book.setCopies(book.getCopies() - 1);
                 book.getPendingBorrowers().add(student.getAccount().getAccountId());
                 student.getPendingBooks().add(book);
 
+                // Save changes
                 saveStudentAccountChanges(student);
                 saveBookChanges(books);
                 return true;
             }
         }
-
         return false;
     }
 
+    /**
+     * Removes a pending book request of a student
+     */
     public boolean removePending(String bookId, Student student) {
-
         LinkedList<Book> books = getBooks();
 
-
         for (Book book : books) {
-
+            // Increase available copies, remove student from pending list
             if (book.getBookId().equals(bookId)) {
                 book.setCopies(book.getCopies() + 1);
                 book.getPendingBorrowers().remove(student.getAccount().getAccountId());
+                // remove book from pending list
                 removeBookOnStudentPending(bookId, student);
 
+                // Save changes
                 saveStudentAccountChanges(student);
                 saveBookChanges(books);
                 return true;
             }
         }
-
         return false;
     }
 
+    /**
+     * Remove the book from the pending list of student
+     */
     private void removeBookOnStudentPending(String bookId, Student student) {
         for(int i = 0 ; i < student.getPendingBooks().size(); i++) {
             if(student.getPendingBooks().get(i).getBookId().equals(bookId)) {
@@ -133,12 +134,13 @@ public class DataModel {
         }
     }
 
+    /**
+     * Add the borrowed books of a student
+     */
     public boolean addBorrowed(String bookId, Student student) {
-
         LinkedList<Book> books = getBooks();
 
         for (Book book : books) {
-
             if (book.getBookId().equals(bookId)) {
                 book.getCurrentBorrowers().add(student.getAccount().getAccountId());
                 book.getPendingBorrowers().remove(student.getAccount().getAccountId());
@@ -150,17 +152,18 @@ public class DataModel {
                 return true;
             }
         }
-
         return false;
     }
 
 
+    /**
+     * Remove the borrowed books of a student
+     */
     public boolean removeBorrowed(String bookId, Student student, boolean isAdmin) {
         LinkedList<Book> books = getBooks();
 
         for (Book book : books) {
             if (book.getBookId().equals(bookId)) {
-
                 book.getCurrentBorrowers().remove(student.getAccount().getAccountId());
 
                 if (isAdmin) {
@@ -169,7 +172,6 @@ public class DataModel {
                     book.getPendingBookReturners().add(student.getAccount().getAccountId());
                     student.getPendingReturnBook().add(book);
                 }
-
                 book.setCopies(book.getCopies() + 1);
                 student.getBorrowedBooks().removeIf(b -> b.getBookId().equals(bookId));
 
@@ -178,31 +180,32 @@ public class DataModel {
                 return true;
             }
         }
-
         return false;
     }
 
+    /**
+     * Retrieve the pending return books
+     */
     public boolean retrievePendingReturnBook(String bookId, Student student) {
         LinkedList<Book> books = getBooks();
 
         for (Book book : books) {
-
             if (book.getBookId().equals(bookId)) {
-
                 book.getPendingBookReturners().remove(student.getAccount().getAccountId());
                 book.getPrevBookBorrowers().add(new PrevBorrower(Date.valueOf(LocalDate.now()).toString(), student.getAccount().getAccountId(), student.getAccount().getFirstName() + " " + student.getAccount().getLastName()));
                 student.getPendingReturnBook().removeIf(b -> b.getBookId().equals(bookId));
-
 
                 saveStudentAccountChanges(student);
                 saveBookChanges(books);
                 return true;
             }
         }
-
         return false;
     }
 
+    /**
+     * Saves changes to the student's account
+     */
     public void saveStudentAccountChanges(Student student) {
         LinkedList<Student> accounts = getStudentAccounts(getBooks());
 
@@ -219,17 +222,26 @@ public class DataModel {
 
     }
 
+    /**
+     * Save changes to the list of student's accounts
+     */
     public void saveStudentAccountChanges(LinkedList<Student> students) {
         saveJSON(gson.toJson(transformStudent(students)), accountJSONfilePath);
     }
 
+    /**
+     * Saves changes to the list of books.
+     */
     public void saveBookChanges(LinkedList<Book> books) {
         saveJSON(gson.toJson(transformedBooks(books)), bookJSONPath);
     }
 
-
+    /**
+     * Saves changes to a specific book
+     * @param book
+     * @return
+     */
     public boolean saveBookChanges(Book book) {
-
         LinkedList<Book> books = getBooks();
 
         for (int i = 0; i < books.size(); i++) {
@@ -240,34 +252,28 @@ public class DataModel {
                 saveJSON(gson.toJson(transformedBooks(books)), bookJSONPath);
                 return true;
             }
-
         }
-
         return false;
     }
 
-
+    /**
+     * Retrieves a list of student accounts from JSON data.
+     */
     public LinkedList<Student> getStudentAccounts(LinkedList<Book> allBooks) {
         try (FileReader reader = new FileReader(accountJSONfilePath)) {
             Type linkedListType = new TypeToken<LinkedList<TransformedStudent>>() {}.getType();
             LinkedList<TransformedStudent> studentsFromJson = gson.fromJson(reader, linkedListType);
 
-
             LinkedList<Student> students = new LinkedList<>();
 
             studentsFromJson.forEach(transformedStudent -> {
-
-
                 LinkedList<String> borrowed = transformedStudent.getBorrowedBooks().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
                 LinkedList<String> pending = transformedStudent.getPendingBooks().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
                 LinkedList<String> pendingReturn = transformedStudent.getPendingReturnBook().stream().map(current -> current.get("id").toString().replace("\"", "")).collect(Collectors.toCollection(LinkedList::new));
 
-
                 LinkedList<Book> currentBorrowedBooks = new LinkedList<>();
                 LinkedList<Book> pendingBooks = new LinkedList<>();
                 LinkedList<Book> pendingReturnBooks = new LinkedList<>();
-
-
 
                 allBooks.forEach(book -> {
                     for (String string : borrowed) {
@@ -277,9 +283,7 @@ public class DataModel {
                     }
                 });
 
-
                 allBooks.forEach(book -> {
-
                     for (String string : pending) {
                         if (book.getBookId().equals(string)) {
                             pendingBooks.add(book);
@@ -288,7 +292,6 @@ public class DataModel {
                 });
 
                 allBooks.forEach(book -> {
-
                     for (String string : pendingReturn) {
                         if (book.getBookId().equals(string)) {
                             pendingReturnBooks.add(book);
@@ -298,8 +301,6 @@ public class DataModel {
 
                 students.add(new Student(new Account(transformedStudent.getAccountId(), transformedStudent.getUserName(), transformedStudent.getFirstName(), transformedStudent.getLastName(), transformedStudent.getEmail(), transformedStudent.getPassword(), transformedStudent.isBanned()), transformedStudent.getTotalBorrowedBooks(), pendingBooks, currentBorrowedBooks, pendingReturnBooks));
             });
-
-
             return students;
         } catch (IOException e) {
             System.err.println(e.getMessage());
@@ -307,7 +308,9 @@ public class DataModel {
         }
     }
 
-
+    /**
+     * Transforms a list of books into a list of transformed books objects
+     */
     protected LinkedList<TransformedBook> transformedBooks(LinkedList<Book> books) {
 
         LinkedList<TransformedBook> transformedBooks = new LinkedList<>();
@@ -319,6 +322,7 @@ public class DataModel {
             LinkedList<JsonObject> pendingBookReturner = new LinkedList<>();
             LinkedList<JsonObject> prevBorrowers = new LinkedList<>();
 
+            // Convert lists of borrowers and returners to JSON objects
             book.getPendingBorrowers().forEach(student -> {
                 JsonObject id = new JsonObject();
                 id.add("id", gson.toJsonTree(student));
@@ -345,14 +349,15 @@ public class DataModel {
                 prevBorrowers.add(prev);
             });
 
+            // Create the transformed book object and add to list
             transformedBooks.add(new TransformedBook(book.getBookId(), book.getBookTitle(), book.getAuthor(), book.getGenre(), book.getShortDescription(), book.getImagePath(), book.getCopies(), currentBorrowers, prevBorrowers, pendingBorrowers, pendingBookReturner));
-
         }
-
-
         return transformedBooks;
     }
 
+    /**
+     * Transforms a list of students into a list of TransformedStudent objects.
+     */
     protected LinkedList<TransformedStudent> transformStudent(LinkedList<Student> accounts) {
         LinkedList<TransformedStudent> transformed = new LinkedList<>();
 
@@ -380,11 +385,8 @@ public class DataModel {
                 pendingReturn.add(id);
             });
 
-
             transformed.add(new TransformedStudent(studentAccount.getAccountId(), studentAccount.getUserName(), studentAccount.getFirstName(), studentAccount.getLastName(), studentAccount.getEmail(), studentAccount.getPassword(), studentAccount.getIsBanned(), student.getTotalBorrowedBooks(), pending, borrowed, pendingReturn));
         }
         return transformed;
     }
-
-
 }
